f0b5b9403e2ff28ab408a79c6e7b627b48596f2f
javiergbg3 <javiergonzalezgarrapa@gmail.com>
2023-03-15 22:34:37 +0100

    s

--- tablas_r_postgres.sql
+++ tablas_r_postgres.sql
@@ -15,7 +15,7 @@ CREATE TABLE Clientes (
     DNI         VARCHAR(9) PRIMARY KEY,
     nombre      VARCHAR(50) NOT NULL,
     apellidos   VARCHAR(50) NOT NULL,
-    edad        INTEGER NOT NULL,
+    edad        BIGINT NOT NULL,
     direccion   VARCHAR(100) NOT NULL,
     email       VARCHAR(100),
     telefono    VARCHAR(20) NOT NULL
@@ -23,18 +23,18 @@ CREATE TABLE Clientes (
 
 -- Creación de tabla Oficinas
 CREATE TABLE Oficinas (
-    codigo_oficina  INTEGER PRIMARY KEY,
+    codigo_oficina  BIGINT PRIMARY KEY,
     direccion       VARCHAR(100) NOT NULL,
     telefono        VARCHAR(20) NOT NULL
 );
 
 -- Creación de tabla Cuentas
 CREATE TABLE Cuentas (
-    numero_cuenta   INTEGER PRIMARY KEY,
+    numero_cuenta   BIGINT PRIMARY KEY,
     IBAN            VARCHAR(4) NOT NULL,
     fecha_creacion  DATE NOT NULL,
     saldo_actual    NUMERIC(10,2) DEFAULT 0.00,
-    sucursal        INTEGER REFERENCES Oficinas(codigo_oficina),
+    sucursal        BIGINT REFERENCES Oficinas(codigo_oficina),
     tipo_cuenta     VARCHAR(10) NOT NULL,
     CONSTRAINT      tipo_cuenta_check CHECK (tipo_cuenta IN ('corriente', 'ahorro')),
     CONSTRAINT      saldo_actual_check CHECK (saldo_actual >= 0.00)
@@ -42,7 +42,7 @@ CREATE TABLE Cuentas (
 
 -- Creación de tabla Titulares
 CREATE TABLE Titulares (
-    numero_cuenta   INTEGER REFERENCES Cuentas(numero_cuenta),
+    numero_cuenta   BIGINT REFERENCES Cuentas(numero_cuenta),
     DNI_titular     VARCHAR(9) REFERENCES Clientes(DNI),
     interes         NUMERIC(3,2) NOT NULL,  
     PRIMARY KEY (numero_cuenta, DNI_titular)
@@ -53,9 +53,9 @@ CREATE TABLE Operaciones (
     id_operacion            SERIAL PRIMARY KEY,
     fecha_hora              TIMESTAMP NOT NULL,
     cantidad                NUMERIC(10,2) NOT NULL,
-    numero_cuenta_origen    INTEGER REFERENCES Cuentas(numero_cuenta),
-    numero_cuenta_destino   INTEGER REFERENCES Cuentas(numero_cuenta),
-    sucursal                INTEGER REFERENCES Oficinas(codigo_oficina),
+    numero_cuenta_origen    BIGINT REFERENCES Cuentas(numero_cuenta),
+    numero_cuenta_destino   BIGINT REFERENCES Cuentas(numero_cuenta),
+    sucursal                BIGINT REFERENCES Oficinas(codigo_oficina),
     descripcion             VARCHAR(200),
     tipo_operacion          VARCHAR(15) NOT NULL,
     CONSTRAINT              tipo_operacion_check CHECK (tipo_operacion IN ('ingreso', 'retirada', 'transferencia'))
