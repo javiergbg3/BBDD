1790b0b95df75c172255a87d411f3453dbd6d511
javiergbg3 <79220834+javiergbg3@users.noreply.github.com>
2023-03-15 21:29:22 +0100

    Add files via upload

--- tablas_r_postgres.sql
+++ tablas_r_postgres.sql
@@ -3,7 +3,7 @@ CREATE TABLE Clientes (
     DNI         VARCHAR(9) PRIMARY KEY,
     nombre      VARCHAR(50) NOT NULL,
     apellidos   VARCHAR(50) NOT NULL,
-    edad        INTEGER(3) NOT NULL,
+    edad        INTEGER NOT NULL,
     direccion   VARCHAR(100) NOT NULL,
     email       VARCHAR(100),
     telefono    VARCHAR(20) NOT NULL
@@ -11,18 +11,18 @@ CREATE TABLE Clientes (
 
 -- Creación de tabla Oficinas
 CREATE TABLE Oficinas (
-    codigo_oficina  INTEGER(4) PRIMARY KEY,
+    codigo_oficina  INTEGER PRIMARY KEY,
     direccion       VARCHAR(100) NOT NULL,
     telefono        VARCHAR(20) NOT NULL
 );
 
 -- Creación de tabla Cuentas
 CREATE TABLE Cuentas (
-    numero_cuenta   INTEGER(20) PRIMARY KEY,
+    numero_cuenta   INTEGER PRIMARY KEY,
     IBAN            VARCHAR(4) NOT NULL,
     fecha_creacion  DATE NOT NULL,
-    saldo_actual    DECIMAL(10,2) DEFAULT 0.00,
-    sucursal        INTEGER(4) REFERENCES Oficinas(codigo_oficina),
+    saldo_actual    NUMERIC(10,2) DEFAULT 0.00,
+    sucursal        INTEGER REFERENCES Oficinas(codigo_oficina),
     tipo_cuenta     VARCHAR(10) NOT NULL,
     CONSTRAINT      tipo_cuenta_check CHECK (tipo_cuenta IN ('corriente', 'ahorro')),
     CONSTRAINT      saldo_actual_check CHECK (saldo_actual >= 0.00)
@@ -30,20 +30,20 @@ CREATE TABLE Cuentas (
 
 -- Creación de tabla Titulares
 CREATE TABLE Titulares (
-    numero_cuenta   INTEGER(20) REFERENCES Cuentas(numero_cuenta),
+    numero_cuenta   INTEGER REFERENCES Cuentas(numero_cuenta),
     DNI_titular     VARCHAR(9) REFERENCES Clientes(DNI),
-    interes         INTEGER(3,2) NOT NULL,  
+    interes         NUMERIC(3,2) NOT NULL,  
     PRIMARY KEY (numero_cuenta, DNI_titular)
 );
 
 -- Creación de tabla Operaciones
 CREATE TABLE Operaciones (
-    id_operacion            INTEGER(10) SERIAL PRIMARY KEY,
+    id_operacion            INTEGER SERIAL PRIMARY KEY,
     fecha_hora              TIMESTAMP NOT NULL,
-    cantidad                DECIMAL(10,2) NOT NULL,
+    cantidad                NUMERIC(10,2) NOT NULL,
     numero_cuenta_origen    VARCHAR(20) REFERENCES Cuentas(numero_cuenta),
     numero_cuenta_destino   VARCHAR(20) REFERENCES Cuentas(numero_cuenta),
-    sucursal                INTEGER(4) REFERENCES Oficinas(codigo_oficina),
+    sucursal                INTEGER REFERENCES Oficinas(codigo_oficina),
     descripcion             VARCHAR(200),
     tipo_operacion          VARCHAR(15) NOT NULL,
     CONSTRAINT              tipo_operacion_check CHECK (tipo_operacion IN ('ingreso', 'retirada', 'transferencia'))
