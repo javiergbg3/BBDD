2b1ecd2937f717f2ddc3746886ac9edfaee9b256
javiergbg3 <javiergonzalezgarrapa@gmail.com>
2023-03-21 19:58:07 +0100

    a

--- pruebas_or_postgres.sql
+++ pruebas_or_postgres.sql
@@ -7,3 +7,5 @@ SELECT numero_cuenta FROM ahorro;
 SELECT numero_cuenta FROM corriente;
 
 SELECT numero_cuenta FROM ONLY cuentas;
+
+SELECT cuentas.numero_cuenta FROM cuentas, Titulares, clientes WHERE dni = DNI_titular AND dni = '34567890D' AND cuentas.numero_cuenta = Titulares.numero_cuenta;
