364e9b1f4fb0ae27f34ba191be9d3d12e0120e85
javiergbg3 <javiergonzalezgarrapa@gmail.com>
2023-04-18 20:51:57 +0200

    aaa

--- Almonte_tablas_2.sql
+++ Almonte_tablas_2.sql
@@ -11,13 +11,13 @@ DROP TABLE IF EXISTS Proveedor;
 
 
 -- Creación de tabla Proveedor
-CREATE TABLE Proveedor (
+CREATE TABLE esquema2.Proveedor (
     nombre          VARCHAR(50) PRIMARY KEY,
     telefono        VARCHAR(20) NOT NULL
 );
 
 -- Creación de tabla Instalacion
-CREATE TABLE Instalacion(
+CREATE TABLE esquema2.Instalacion(
     id_instalacion  SERIAL PRIMARY KEY,
     direccion       VARCHAR(100) NOT NULL,
     telefono        VARCHAR(20) NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE Instalacion(
 );
 
 -- Creación de tabla Item
-CREATE TABLE Item (
+CREATE TABLE esquema2.Item (
     id_item         SERIAL PRIMARY KEY,
     nombre          VARCHAR(30) NOT NULL,
     precio          INTEGER NOT NULL,
@@ -34,14 +34,14 @@ CREATE TABLE Item (
 );
 
 -- Creación de tabla Tiene
-CREATE TABLE Tiene (
+CREATE TABLE esquema2.Tiene (
     id_producto            SERIAL REFERENCES Item(id_item),
     id_establecimiento     SERIAL REFERENCES Instalacion(id_instalacion),
     PRIMARY KEY (id_producto, id_establecimiento)
 );
 
 -- Creación de tabla Persona
-CREATE TABLE Persona (
+CREATE TABLE esquema2.Persona (
     id_persona              SERIAL PRIMARY KEY,
     DNI                     VARCHAR(9),
     lugar_trabajo           SERIAL REFERENCES Instalacion(id_instalacion),
