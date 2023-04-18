a3bddb44dba7b978b985b54931c48900de073f70
javiergbg3 <javiergonzalezgarrapa@gmail.com>
2023-04-18 20:52:15 +0200

    aaaa

--- Almonte_tablas_2.sql
+++ Almonte_tablas_2.sql
@@ -30,13 +30,13 @@ CREATE TABLE esquema2.Item (
     nombre          VARCHAR(30) NOT NULL,
     precio          INTEGER NOT NULL,
     tipo            VARCHAR(30) NOT NULL,
-    proveedor_id    VARCHAR(50) REFERENCES Proveedor(nombre)
+    proveedor_id    VARCHAR(50) REFERENCES esquema2.Proveedor(nombre)
 );
 
 -- Creación de tabla Tiene
 CREATE TABLE esquema2.Tiene (
-    id_producto            SERIAL REFERENCES Item(id_item),
-    id_establecimiento     SERIAL REFERENCES Instalacion(id_instalacion),
+    id_producto            SERIAL REFERENCES esquema2.Item(id_item),
+    id_establecimiento     SERIAL REFERENCES esquema2.Instalacion(id_instalacion),
     PRIMARY KEY (id_producto, id_establecimiento)
 );
 
@@ -44,7 +44,7 @@ CREATE TABLE esquema2.Tiene (
 CREATE TABLE esquema2.Persona (
     id_persona              SERIAL PRIMARY KEY,
     DNI                     VARCHAR(9),
-    lugar_trabajo           SERIAL REFERENCES Instalacion(id_instalacion),
+    lugar_trabajo           SERIAL REFERENCES esquema2.Instalacion(id_instalacion),
     nombre                  VARCHAR(30) NOT NULL,
     correo                  VARCHAR(30) NOT NULL,
     telefono                VARCHAR(20) NOT NULL,
